6f71bcad356a7764bc5e24bf507d3b4dca8173fb
mvzan <165400589+cuziycloud@users.noreply.github.com>
2025-03-16 23:49:49 +0700

    update img of products

--- src/customer/components/HomeCarousel/MainCarouselData.js
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -1,9 +1,13 @@
 import j1 from "F:/522h0083_TranMyVan/code/web/ecommerce/src/assets/img/j1.jpg";
 import j2 from "F:/522h0083_TranMyVan/code/web/ecommerce/src/assets/img/j2.jpg";
 import j3 from "F:/522h0083_TranMyVan/code/web/ecommerce/src/assets/img/j3.jpg";
+import j4 from "F:/522h0083_TranMyVan/code/web/ecommerce/src/assets/img/j4.jpg";
+import j5 from "F:/522h0083_TranMyVan/code/web/ecommerce/src/assets/img/j5.jpg";
 
 export const mainCarouselData = [
     { image: j1 },
     { image: j2 },
     { image: j3 },
+    { image: j4 },
+    { image: j5 },
 ];
