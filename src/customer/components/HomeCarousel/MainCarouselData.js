c6c6b39771772dfd3311678262e3d33b53f7878b
mvzan <165400589+cuziycloud@users.noreply.github.com>
2025-03-16 23:46:59 +0700

    Second commit

---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,9 @@
+import j1 from "F:/522h0083_TranMyVan/code/web/ecommerce/src/assets/img/j1.jpg";
+import j2 from "F:/522h0083_TranMyVan/code/web/ecommerce/src/assets/img/j2.jpg";
+import j3 from "F:/522h0083_TranMyVan/code/web/ecommerce/src/assets/img/j3.jpg";
+
+export const mainCarouselData = [
+    { image: j1 },
+    { image: j2 },
+    { image: j3 },
+];
