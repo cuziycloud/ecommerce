b7af0bb9b903202d6b0fd9b3857d735b8b62c803
mvzan <165400589+cuziycloud@users.noreply.github.com>
2025-03-19 21:47:17 +0700

    product card

--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
-  )
-}
+    <div className="productCard w-[15rem] m-3 transition-all cursor-pointer">
+      <div className="h-[20rem]">
+        <img
+          className="h-full w-full object-cover object-left-top"
+          src=""
+          alt=""
+        />
+      </div>
 
-export default ProductCard
+      <div className="textPart bg-white mt-4 flex flex-col">
+        <p className="font-bold opacity-60">Name of product</p>
+        <p>This is description about my product. (Coming soon)</p>
+
+        <div className="flex items-center space-x-3 mt-2">
+          <p className="font-semibold">30$</p>
+          <p className="line-through opacity-50">100$</p>
+          <p className="text-green-600 font-semibold">70% off</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
