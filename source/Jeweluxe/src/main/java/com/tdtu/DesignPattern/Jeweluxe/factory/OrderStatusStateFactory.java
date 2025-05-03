e1da4fee3ae9e798c186aa44d3340e7f8f3b831d
mvzan <165400589+cuziycloud@users.noreply.github.com>
2025-05-03 21:00:32 +0700

    fix factory m p

--- source/Jeweluxe/src/main/java/com/tdtu/DesignPattern/Jeweluxe/factory/OrderStatusStateFactory.java
+++ source/Jeweluxe/src/main/java/com/tdtu/DesignPattern/Jeweluxe/factory/OrderStatusStateFactory.java
@@ -1,36 +1,24 @@
 package com.tdtu.DesignPattern.Jeweluxe.factory;
 
-import com.tdtu.DesignPattern.Jeweluxe.state.CancelledState;
-import com.tdtu.DesignPattern.Jeweluxe.state.DeliveredState;
-import com.tdtu.DesignPattern.Jeweluxe.state.InProgressState;
-import com.tdtu.DesignPattern.Jeweluxe.state.OrderReceivedState;
-import com.tdtu.DesignPattern.Jeweluxe.state.OrderStatusState; 
-import com.tdtu.DesignPattern.Jeweluxe.state.ProductPackedState;
-import com.tdtu.DesignPattern.Jeweluxe.state.ShippedState;
+import com.tdtu.DesignPattern.Jeweluxe.state.*;
 import com.tdtu.DesignPattern.Jeweluxe.util.OrderStatus;
+import org.springframework.stereotype.Component; 
 
-// Factory Class: Chịu trách nhiệm tạo các instance OrderStatusState
-public class OrderStatusStateFactory {
+@Component 
+public class OrderStatusStateFactory implements OrderStatusStateCreator {
 
-    //Tạo đối tượng State dựa trên Enum OrderStatus/ Đây là điểm tạo đối tượng tập trung
-    public static OrderStatusState createState(OrderStatus statusEnum) {
+    @Override
+    public OrderStatusState createStatusState(OrderStatus statusEnum) {
         if (statusEnum == null) {
-            return null;
+            throw new IllegalArgumentException("OrderStatus enum ko được null để tạo State.");
         }
-
         switch (statusEnum) {
-            case IN_PROGRESS:
-                return new InProgressState();
-            case ORDER_RECEIVED:
-                return new OrderReceivedState();
-            case PRODUCT_PACKED:
-                return new ProductPackedState();
-            case OUT_FOR_DELIVERY: 
-                return new ShippedState();
-            case DELIVERED:
-                return new DeliveredState();
-            case CANCELLED:
-                return new CancelledState();
+            case IN_PROGRESS: return new InProgressState();
+            case ORDER_RECEIVED: return new OrderReceivedState();
+            case PRODUCT_PACKED: return new ProductPackedState();
+            case OUT_FOR_DELIVERY: return new ShippedState();
+            case DELIVERED: return new DeliveredState();
+            case CANCELLED: return new CancelledState();
             default:
                 throw new IllegalArgumentException("Trạng thái chưa được hỗ trợ: " + statusEnum);
         }
